e222dd8ed8cf6ff125c1da4e360ff397f6275b20
MMotti <matthew.w.motti@gmail.com>
2018-03-22 15:06:27 +0000

    Added dnsmasq config file for supported wildcards (*.something)

---
+++ resources/build_dnsmasq_hosts.ps1
@@ -0,0 +1,27 @@
+﻿# Define output file
+$out_file = "$PSScriptRoot\wildcard_hosts_dnsmasq.conf"
+
+# Get the wildcard host input file
+$wcard_hosts = (Get-Content "$PSScriptRoot\wildcard_hosts.txt") -match "(\*\.)"
+
+# Remove *.
+$wcard_hosts = $wcard_hosts -replace "\*\."
+
+# Create empty array
+$dnsmasq_arr = @()
+
+# For each wildcard
+foreach($wcard in $wcard_hosts)
+{
+    # Convert to dnsmasq format
+    $wcard = "address=/$wcard/192.168.1.200"
+    
+    # Add to array
+    $dnsmasq_arr += $wcard
+}
+
+# Join on a new line
+$dnsmasq_arr = $dnsmasq_arr -join "`n"
+
+# Output to file
+[System.IO.File]::WriteAllText($out_file,$dnsmasq_arr)
