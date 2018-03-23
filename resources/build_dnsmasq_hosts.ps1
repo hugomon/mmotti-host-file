4d296de95ff8a497a2d5b8e856dbcb7a0bfe0840
MMotti <matthew.w.motti@gmail.com>
2018-03-23 18:41:34 +0000

    dnsmasq script update and host-file correction Now includes conditional output (same as the normal host-file script). Fixed the redirection (it previously pointed to my pi-hole IP)

--- resources/build_dnsmasq_hosts.ps1
+++ resources/build_dnsmasq_hosts.ps1
@@ -1,11 +1,20 @@
-﻿# Define output file
-$out_file = "$PSScriptRoot\wildcard_hosts_dnsmasq.conf"
+﻿# Get the script's parent directory
+$parent_dir   = (Get-Item $PSScriptRoot).Parent.FullName
+# Check whether we are running from the git repo
+$isGitRepo    = Get-ChildItem -Attributes "Hidden" -Filter ".git" $parent_dir
+
+# Conditional output location
+if($isGitRepo)
+{
+    $out_file     = "$parent_dir\wildcard_hosts_dnsmasq.conf"
+}
+else
+{
+    $out_file     = "$PSScriptRoot\wildcard_hosts_dnsmasq.conf"
+}
 
 # Get the wildcard host input file
-$wcard_hosts = (Get-Content "$PSScriptRoot\wildcard_hosts.txt") -match "(\*\.)"
-
-# Remove *.
-$wcard_hosts = $wcard_hosts -replace "\*\."
+$wcard_hosts = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mmotti/mmotti-host-file/master/wildcard_standard_hosts.txt").Content -split "\n" -match "(\*\.)" -replace "\*\."
 
 # Create empty array
 $dnsmasq_arr = @()
@@ -14,7 +23,7 @@ $dnsmasq_arr = @()
 foreach($wcard in $wcard_hosts)
 {
     # Convert to dnsmasq format
-    $wcard = "address=/$wcard/192.168.1.200"
+    $wcard = "address=/$wcard/0.0.0.0"
     
     # Add to array
     $dnsmasq_arr += $wcard
@@ -22,6 +31,9 @@ foreach($wcard in $wcard_hosts)
 
 # Join on a new line
 $dnsmasq_arr = $dnsmasq_arr -join "`n"
+
+# Output to screen
+Write-Output $dnsmasq_arr
 
 # Output to file
 [System.IO.File]::WriteAllText($out_file,$dnsmasq_arr)
